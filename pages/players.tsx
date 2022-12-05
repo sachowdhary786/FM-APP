732d45d43e3a8299b9ff55e8eaba252d6c440e76
Saad <sachowdhary786@gmail.com>
2022-12-05 22:06:55 +0000

    Build of players page

---
+++ pages/players.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './players.scss';
+import Layout from '../src/common/layout/layout';
+
+export default function Players() {
+    return (
+        <Layout>
+            <div></div>
+        </Layout>
+    )
+}
