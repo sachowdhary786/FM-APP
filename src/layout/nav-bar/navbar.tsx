77dfc946c1463e8ccb77d073d0408217c7978bd4
Saad <sachowdhary786@gmail.com>
2022-12-08 00:41:51 +0000

    Change tabindex to tabIndex

--- src/layout/nav-bar/navbar.tsx
+++ src/layout/nav-bar/navbar.tsx
@@ -7,17 +7,17 @@ function NavBar() {
     return (
         <div className={style['nav-container']}>
             <Link className={style['nav-link']} href="/">
-                <button className={style['nav-button']} tabindex="-1">
+                <button className={style['nav-button']} tabIndex="-1">
                     Home
                 </button>
             </Link>
             <Link className={style['nav-link']} href="/players">
-                <button className={style['nav-button']} tabindex="-1">
+                <button className={style['nav-button']} tabIndex="-1">
                     Players
                 </button>
             </Link>
             <Link className={style['nav-link']} href="/about">
-                <button className={style['nav-button']} tabindex="-1">
+                <button className={style['nav-button']} tabIndex="-1">
                     About
                 </button>
             </Link>
