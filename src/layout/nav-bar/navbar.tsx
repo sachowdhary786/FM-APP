fd3af68fa26278d107eeec2d01ebbcf41d5ee5a8
Saad <sachowdhary786@gmail.com>
2022-12-08 00:44:12 +0000

    update string to number

--- src/layout/nav-bar/navbar.tsx
+++ src/layout/nav-bar/navbar.tsx
@@ -7,21 +7,21 @@ function NavBar() {
     return (
         <div className={style['nav-container']}>
             <Link className={style['nav-link']} href="/">
-                <button className={style['nav-button']} tabIndex="-1">
+                <button className={style['nav-button']} tabIndex={-1}>
                     Home
                 </button>
             </Link>
             <Link className={style['nav-link']} href="/players">
-                <button className={style['nav-button']} tabIndex="-1">
+                <button className={style['nav-button']} tabIndex={-1}>
                     Players
                 </button>
             </Link>
             <Link className={style['nav-link']} href="/about">
-                <button className={style['nav-button']} tabIndex="-1">
+                <button className={style['nav-button']} tabIndex={-1}>
                     About
                 </button>
             </Link>
-        </div>
+        </div >
     )
 }
 
