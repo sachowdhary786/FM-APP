970863cfc945eb2face8f4ac74414bbbd3bfc263
Saad <sachowdhary786@gmail.com>
2022-12-08 12:28:31 +0000

    Fix server connection breaking

--- server/routes/record.js
+++ server/routes/record.js
@@ -6,11 +6,11 @@ const dbo = require('../db/conn');
 
 const ObjectId = require('mongodb').ObjectId;
 
-recordRoutes.route('/record').get(function (res) {
+recordRoutes.route('/record').get(function (req, res) {
   let db_connect = dbo.getDb('players');
   db_connect.collection('arsenal').find({}).toArray(function (err, result) {
     if (err) throw err;
-    res.json(result);
+    res.json(result)
   })
 });
 
