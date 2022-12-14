62545b768d1b083611e71fae10d964478d2f7b8a
Saad <sachowdhary786@gmail.com>
2022-12-14 15:53:41 +0000

    Remove Apollo for now

--- server/types/context.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { User } from "../models/user.model";
-
-export type Context = {
-  req: NextApiRequest;
-  res: NextApiResponse;
-  // eslint-disable-next-line no-unused-vars
-  deserializeUser: (req: NextApiRequest, res: NextApiResponse) => Promise<User | undefined>;
-};
